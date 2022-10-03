cee257a6beb0bd3486a346b52d0e86fc9c82c2f8
陳建佑 <jianyou224@gmail.com>
2022-10-03 14:18:17 +0800

    jwt sample

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
 var router = express.Router();
 
 // '/api'
@@ -58,6 +59,54 @@ router.get('/mdTest',midTest, function(req, res, next) {
   res.send('hello');
   res.end();
 });
-//---------------------------------
 
+// json web token test---------------------------------
+
+router.get('/tokTest', function(req, res) {
+  res.json({
+    text: 'my api!'
+  });
+});
+
+// login後取得token sign()
+router.post('/login', function(req, res) {
+  // 假user資料
+  const user = {id:3};
+  const token = jwt.sign({user}, 'my_secret_key');
+  res.json({
+    token: token
+  });
+});
+
+// 要夾帶token才能get到資料  verify()
+router.get('/protected',ensureToken, function(req, res) {
+  jwt.verify(req.token, 'my_secret_key', function(err, data) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        text: 'this is protected',
+        data:data
+      })
+    }
+  })
+  res.json({
+    text: 'this is protected'
+  })
+})
+
+// 守門員
+function ensureToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+// ---------------------------------
 module.exports = router;
